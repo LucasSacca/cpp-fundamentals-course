6bd989370904b4cf7e5d212ceef2f876f178695b
Lucas Sacca <150452407+LucasSacca@users.noreply.github.com>
2026-03-26 08:48:46 -0300

    Add main.cpp

---
+++ lessons/lesson07_oop/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "Circle.cpp"
+
+int main()
+{
+    Circle circle;
+    circle.setRadius(3);
+    
+    printf("\n\nRaio = %f", circle.getRadius());
+    printf("\nÁrea = %f", circle.getArea());
+    printf("\nPerímetro = %f", circle.getPerimeter());
+
+    return 0
+}
